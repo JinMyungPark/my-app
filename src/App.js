d0571ee8b99d69ff70e4f9dc390a5e6cf904fb1b
JinMyungPark <dcansjm@gmail.com>
2023-12-12 17:11:36 +0900

    mui적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. 오케이.
@@ -17,6 +34,14 @@ function App() {
         >
           Learn React
         </a>
+        <Container maxWidth="sm">
+          <Box sx={{ my: 4 }}>
+            <Typography variant="h4" component="h1" gutterBottom>
+              Create React App v5 example
+            </Typography>
+            <Copyright />
+          </Box>
+        </Container>
       </header>
     </div>
   );
